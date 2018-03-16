582bf5a88b21d14b618f11e47b323ad15cf034b9
Rockford Lhotka <rocky@lhotka.net>
2018-03-15 21:35:14 -0700

    Initial add of demo app.

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Csla;
+
+namespace MyFormsApp
+{
+  public class Person : INotifyPropertyChanged
+  {
+    private string _name;
+    public string Name
+    {
+      get { return _name; }
+      set { _name = value; OnPropertyChanged("Name"); }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string name)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+  }
+
+  // CSLA Person class - but doesn't work just yet :(
+  //
+  //[Serializable]
+  //public class Person : BusinessBase<Person>
+  //{
+  //  public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(c => c.Name);
+  //  public string Name
+  //  {
+  //    get { return GetProperty(NameProperty); }
+  //    set { SetProperty(NameProperty, value); }
+  //  }
+
+  //  protected override void DataPortal_Create()
+  //  {
+  //    using (BypassPropertyChecks)
+  //    {
+  //      Name = "Teresa";
+  //    }
+  //  }
+  //}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace MyFormsApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Initialize Xamarin.Forms
+      Forms.Init();
+
+      // Create the UI
+      var page = new Page1();
+      var obj = new Person { Name = "Jessica Jones" };
+      page.BindingContext = obj;
+
+      // Publish a root element to be displayed
+      UI.Publish("/", page.GetOouiElement());
+    }
+  }
+}
